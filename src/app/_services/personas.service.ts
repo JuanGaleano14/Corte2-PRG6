1ad8dd88424dc6e5ce579780e822fbbffe1086d3
Juan Pablo Galeano S <56057491+JuanGaleano14@users.noreply.github.com>
2020-04-21 22:53:01 -0500

    Actividad 3 CR

--- src/app/_services/personas.service.ts
+++ src/app/_services/personas.service.ts
@@ -14,6 +14,14 @@ export class PersonasService {
 
   }
 
+  guardarPersonas(nombre: string, edad: string, apodo: string, imagen: string, posicion: string, estado: string){
+   
+    return this.http.get('https://localhost/angulardb/guardar.php?nombre=' + nombre +
+     '&edad=' + edad + '&apodo=' + apodo + '&foto=' + imagen + '&posicion=' + posicion + '&estado='
+      + estado);
+
+  }
+
 }
 
 
--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Personas} from '../../interfaces/personas.interface';
 import { PersonasService } from './../../_services/personas.service';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-tabla',
@@ -11,6 +12,13 @@ export class TablaComponent implements OnInit {
 
   personas: Personas[] = [];
   columnsToDisplay = ['id', 'nombre', 'apellido', 'edad', 'estado' ];
+
+  nombre = new FormControl('');
+  apodo = new FormControl('');
+  posicion = new FormControl('');
+  imagen = new FormControl('');
+  edad = new FormControl('');
+  estado = new FormControl('');
 
   constructor(private servicioPersonas: PersonasService) {
 
@@ -30,6 +38,15 @@ export class TablaComponent implements OnInit {
         console.log(personas);
         this.personas = personas;
       });
+  }
+  
+  guardar() {
+
+    this.servicioPersonas.guardarPersonas(this.nombre.value, this.edad.value, this.apodo.value, this.imagen.value,
+      this.posicion.value, this.estado.value).subscribe((snap) => {
+      console.log(snap);
+    });
+    this.ngOnInit();
   }
 
 }
